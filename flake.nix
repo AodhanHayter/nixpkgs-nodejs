188fb920c9a9e791f27ee204ed1d856f62529a9f
Aodhan Hayter <aodhan.hayter@gmail.com>
2024-04-05 14:31:52 -0600

    Fix dist directory

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
           nodejs_6 = pkgs.stdenv.mkDerivation {
             name = "nodejs6.17.1";
             src = pkgs.fetchurl {
-              url = "https://nodejs.org/dist/v18.15.0/node-v6.17.1-darwin-x64.tar.gz";
+              url = "https://nodejs.org/dist/v6.17.1/node-v6.17.1-darwin-x64.tar.gz";
               sha256 = "sha256-vTAqaJw8NOK2HYa5feZtJqM1iBoXrwm2oKS7EBnfVuQ=";
             };
             installPhase = ''
