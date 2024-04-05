b454292c5a6f5380f48af3f1a781409efc9b6453
Aodhan Hayter <aodhan.hayter@gmail.com>
2024-04-05 14:53:27 -0600

    add flake-compat

--- flake.nix
+++ flake.nix
@@ -3,6 +3,9 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-compat.flake = false;
   };
 
   outputs = inputs@{ flake-parts, ... }:
