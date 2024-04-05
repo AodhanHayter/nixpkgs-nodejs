e163cd295813ffba582ed60c4a20c2d9274f18f9
Aodhan Hayter <aodhan.hayter@gmail.com>
2024-04-05 16:59:57 -0600

    use flake-utils

--- flake.nix
+++ flake.nix
@@ -3,32 +3,32 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-utils.url = "github:numtide/flake-utils";
+
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "aarch64-darwin" "x86_64-linux" ];
-      perSystem = { config, pkgs, system, ... }: {
+  outputs = { flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
         packages = {
           nodejs_6 = pkgs.stdenv.mkDerivation {
-            name = "node";
+            name = "node-6.17.1";
             src = pkgs.fetchurl {
               url = "https://nodejs.org/dist/v6.17.1/node-v6.17.1-darwin-x64.tar.gz";
               sha256 = "sha256-gDPgel2nWa8A23NkwjRPEe7/c7UWR9OZJr+jbyExuZA=";
             };
             installPhase = ''
-              echo "installing nodejs"
+              echo "install nodejs"
               mkdir -p $out
               cp -r ./ $out/
             '';
           };
         };
-      };
-      flake = {};
-    };
+      });
 }
